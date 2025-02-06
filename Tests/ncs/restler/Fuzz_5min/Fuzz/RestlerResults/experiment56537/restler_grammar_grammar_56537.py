3c29da11fef406fe6173d97d6b8a554b82400aea
daniela <danielachavezrejas@outlook.com>
2025-02-06 02:15:56 +0100

    add Tests folder and for ncs

---
+++ Tests/ncs/restler/Fuzz_5min/Fuzz/RestlerResults/experiment56537/restler_grammar_grammar_56537.py
@@ -0,0 +1,149 @@
+""" THIS IS AN AUTOMATICALLY GENERATED FILE!"""
+from __future__ import print_function
+import json
+from engine import primitives
+from engine.core import requests
+from engine.errors import ResponseParsingException
+from engine import dependencies
+req_collection = requests.RequestCollection([])
+# Endpoint: /api/bessj/{n}/{x}, method: Get
+request = requests.Request([
+    primitives.restler_static_string("GET "),
+    primitives.restler_basepath(""),
+    primitives.restler_static_string("/"),
+    primitives.restler_static_string("api"),
+    primitives.restler_static_string("/"),
+    primitives.restler_static_string("bessj"),
+    primitives.restler_static_string("/"),
+    primitives.restler_fuzzable_int("1"),
+    primitives.restler_static_string("/"),
+    primitives.restler_fuzzable_number("1.23"),
+    primitives.restler_static_string(" HTTP/1.1\r\n"),
+    primitives.restler_static_string("Accept: application/json\r\n"),
+    primitives.restler_static_string("Host: localhost:8080\r\n"),
+    primitives.restler_refreshable_authentication_token("authentication_token_tag"),
+    primitives.restler_static_string("\r\n"),
+
+],
+requestId="/api/bessj/{n}/{x}"
+)
+req_collection.add_request(request)
+
+# Endpoint: /api/expint/{n}/{x}, method: Get
+request = requests.Request([
+    primitives.restler_static_string("GET "),
+    primitives.restler_basepath(""),
+    primitives.restler_static_string("/"),
+    primitives.restler_static_string("api"),
+    primitives.restler_static_string("/"),
+    primitives.restler_static_string("expint"),
+    primitives.restler_static_string("/"),
+    primitives.restler_fuzzable_int("1"),
+    primitives.restler_static_string("/"),
+    primitives.restler_fuzzable_number("1.23"),
+    primitives.restler_static_string(" HTTP/1.1\r\n"),
+    primitives.restler_static_string("Accept: application/json\r\n"),
+    primitives.restler_static_string("Host: localhost:8080\r\n"),
+    primitives.restler_refreshable_authentication_token("authentication_token_tag"),
+    primitives.restler_static_string("\r\n"),
+
+],
+requestId="/api/expint/{n}/{x}"
+)
+req_collection.add_request(request)
+
+# Endpoint: /api/fisher/{m}/{n}/{x}, method: Get
+request = requests.Request([
+    primitives.restler_static_string("GET "),
+    primitives.restler_basepath(""),
+    primitives.restler_static_string("/"),
+    primitives.restler_static_string("api"),
+    primitives.restler_static_string("/"),
+    primitives.restler_static_string("fisher"),
+    primitives.restler_static_string("/"),
+    primitives.restler_fuzzable_int("1"),
+    primitives.restler_static_string("/"),
+    primitives.restler_fuzzable_int("1"),
+    primitives.restler_static_string("/"),
+    primitives.restler_fuzzable_number("1.23"),
+    primitives.restler_static_string(" HTTP/1.1\r\n"),
+    primitives.restler_static_string("Accept: application/json\r\n"),
+    primitives.restler_static_string("Host: localhost:8080\r\n"),
+    primitives.restler_refreshable_authentication_token("authentication_token_tag"),
+    primitives.restler_static_string("\r\n"),
+
+],
+requestId="/api/fisher/{m}/{n}/{x}"
+)
+req_collection.add_request(request)
+
+# Endpoint: /api/gammq/{a}/{x}, method: Get
+request = requests.Request([
+    primitives.restler_static_string("GET "),
+    primitives.restler_basepath(""),
+    primitives.restler_static_string("/"),
+    primitives.restler_static_string("api"),
+    primitives.restler_static_string("/"),
+    primitives.restler_static_string("gammq"),
+    primitives.restler_static_string("/"),
+    primitives.restler_fuzzable_number("1.23"),
+    primitives.restler_static_string("/"),
+    primitives.restler_fuzzable_number("1.23"),
+    primitives.restler_static_string(" HTTP/1.1\r\n"),
+    primitives.restler_static_string("Accept: application/json\r\n"),
+    primitives.restler_static_string("Host: localhost:8080\r\n"),
+    primitives.restler_refreshable_authentication_token("authentication_token_tag"),
+    primitives.restler_static_string("\r\n"),
+
+],
+requestId="/api/gammq/{a}/{x}"
+)
+req_collection.add_request(request)
+
+# Endpoint: /api/remainder/{a}/{b}, method: Get
+request = requests.Request([
+    primitives.restler_static_string("GET "),
+    primitives.restler_basepath(""),
+    primitives.restler_static_string("/"),
+    primitives.restler_static_string("api"),
+    primitives.restler_static_string("/"),
+    primitives.restler_static_string("remainder"),
+    primitives.restler_static_string("/"),
+    primitives.restler_fuzzable_int("1"),
+    primitives.restler_static_string("/"),
+    primitives.restler_fuzzable_int("1"),
+    primitives.restler_static_string(" HTTP/1.1\r\n"),
+    primitives.restler_static_string("Accept: application/json\r\n"),
+    primitives.restler_static_string("Host: localhost:8080\r\n"),
+    primitives.restler_refreshable_authentication_token("authentication_token_tag"),
+    primitives.restler_static_string("\r\n"),
+
+],
+requestId="/api/remainder/{a}/{b}"
+)
+req_collection.add_request(request)
+
+# Endpoint: /api/triangle/{a}/{b}/{c}, method: Get
+request = requests.Request([
+    primitives.restler_static_string("GET "),
+    primitives.restler_basepath(""),
+    primitives.restler_static_string("/"),
+    primitives.restler_static_string("api"),
+    primitives.restler_static_string("/"),
+    primitives.restler_static_string("triangle"),
+    primitives.restler_static_string("/"),
+    primitives.restler_fuzzable_int("1"),
+    primitives.restler_static_string("/"),
+    primitives.restler_fuzzable_int("1"),
+    primitives.restler_static_string("/"),
+    primitives.restler_fuzzable_int("1"),
+    primitives.restler_static_string(" HTTP/1.1\r\n"),
+    primitives.restler_static_string("Accept: application/json\r\n"),
+    primitives.restler_static_string("Host: localhost:8080\r\n"),
+    primitives.restler_refreshable_authentication_token("authentication_token_tag"),
+    primitives.restler_static_string("\r\n"),
+
+],
+requestId="/api/triangle/{a}/{b}/{c}"
+)
+req_collection.add_request(request)
